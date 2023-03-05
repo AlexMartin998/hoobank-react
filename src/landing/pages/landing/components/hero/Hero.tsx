f5cfbfe9e512f356f201a9a7da64a13980718657
Alex Martin <alextravc.342@gmail.com>
2023-03-05 08:10:36 -0500

    [ADD]: prevent drag and drop images

--- src/landing/pages/landing/components/hero/Hero.tsx
+++ src/landing/pages/landing/components/hero/Hero.tsx
@@ -14,7 +14,12 @@ const Hero = () => {
         className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
       >
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
-          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+          <img
+            src={discount}
+            alt="discount"
+            className="w-[32px] h-[32px]"
+            draggable={false}
+          />
           <p className={`${styles.paragraph} ml-2`}>
             <span className="text-white">20%</span> Discount For{' '}
             <span className="text-white">1 Month</span> Account
@@ -53,6 +58,8 @@ const Hero = () => {
           src={robot}
           alt="billing"
           className="w-[100%] h-[100%] relative z-[5]"
+          // prevent drag and drop images in html5
+          draggable={false}
         />
 
         {/* gradient */}
