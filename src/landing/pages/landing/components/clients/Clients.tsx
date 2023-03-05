c81e8e730bd71f45889008a4c2d0a0e02a91a55f
Alex Martin <alextravc.342@gmail.com>
2023-03-05 14:37:01 -0500

    [ADD]: clients section

--- src/landing/pages/landing/components/clients/Clients.tsx
+++ src/landing/pages/landing/components/clients/Clients.tsx
@@ -1,5 +1,22 @@
-const Clients = () => {
-  return <div>Clients</div>;
-};
+import { styles, clients } from '../../../../../shared';
+
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map(({ id, logo }) => (
+        <div
+          key={id}
+          className={`flex-1 ${styles.flexCenter} min-w-[120px] sm:min-w-[192px]`}
+        >
+          <img
+            src={logo}
+            alt="client_logo"
+            className="w-[100px] sm:w-[192px] mb-6 sm:mb-0 object-contain"
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
 export default Clients;
