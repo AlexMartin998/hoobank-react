c6b8801040983b3791c7e9aa55517f7635d137ff
Alex Martin <alextravc.342@gmail.com>
2023-03-05 08:34:56 -0500

    [ADD]: billings section

--- src/landing/pages/landing/components/billing/Billing.tsx
+++ src/landing/pages/landing/components/billing/Billing.tsx
@@ -1,5 +1,45 @@
-const Biling = () => {
-  return <div>Biling</div>;
-};
+import { apple, bill, google } from '../../../../../assets';
+import { styles, layout } from '../../../../../shared';
+
+const Biling = () => (
+  <section id="product" className={layout.sectionReverse}>
+    <div className={layout.sectionImgReverse}>
+      <img src={bill} alt="billing" className="w-full h-full relative z-[5]" />
+
+      {/* gradient: relative-image */}
+      <div className="absolute z-[3] -left-1/2 top-0 w-1/2 h-1/2 rounded-full white__gradient" />
+      <div className="absolute z-[0] w-1/2 h-1/2 -left-1/2 bottom-0 rounded-full pink__gradient" />
+    </div>
+
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Easily control your <br className="sm:block hidden" /> billing &
+        invoicing.
+      </h2>
+
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean
+        neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.
+      </p>
+
+      <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+        <button>
+          <img
+            src={apple}
+            alt="apple_store"
+            className="w-[128px] h-[42px] object-contain mr-5"
+          />
+        </button>
+        <button>
+          <img
+            src={google}
+            alt="google_store"
+            className="w-[128px] h-[42px] object-contain"
+          />
+        </button>
+      </div>
+    </div>
+  </section>
+);
 
 export default Biling;
